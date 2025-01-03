1dd3eb34594fb6775da5716fad3db19703024673
dhammond5 <dh3575362@gmail.com>
2025-01-02 23:17:47 -0500

    Udemy-Student-Terraform-Project

---
+++ s3-static/output.tf
@@ -0,0 +1,3 @@
+output "static_website_bucket" {
+  value = aws_s3_bucket_website_configuration.static_website_bucket
+}
---
+++ s3-static/s3.tf
@@ -0,0 +1,62 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 6
+}
+
+resource "aws_s3_bucket" "static_website_bucket" {
+  bucket = "s3-web-bucket-terraform-project-${random_id.bucket_suffix.hex}"
+}
+
+resource "aws_s3_bucket_public_access_block" "static_website_bucket" {
+  bucket = aws_s3_bucket.static_website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "static_website_bucket_policy" {
+  bucket = aws_s3_bucket.static_website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.static_website_bucket.arn}/*"
+      }
+    ]
+  })
+  depends_on = [aws_s3_bucket.static_website_bucket, aws_s3_bucket_public_access_block.static_website_bucket]
+}
+
+resource "aws_s3_bucket_website_configuration" "static_website_bucket" {
+  bucket = aws_s3_bucket.static_website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "index_html" {
+  bucket       = aws_s3_bucket.static_website_bucket.id
+  key          = "index.html"
+  source       = "build/index.html"
+  etag         = filemd5("build/index.html")
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "error_html" {
+  bucket       = aws_s3_bucket.static_website_bucket.id
+  key          = "error.html"
+  source       = "build/error.html"
+  etag         = filemd5("build/error.html")
+  content_type = "text/html"
+}
